0aad06a359c818fb155e3c1ed8638522c0d151f8
Lukas Schlechter / BBS2H20A BBM3H18M <Lukas.Schlechter@edu.bib.de>
2021-10-11 09:16:13 +0200

    lul test 123

---
+++ yoink-test/Shared/yoink_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  yoink_testApp.swift
+//  Shared
+//
+//  Created by Lukas Schlechter / BBS2H20A BBM3H18M on 11.10.21.
+//
+
+import SwiftUI
+
+@main
+struct yoink_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
